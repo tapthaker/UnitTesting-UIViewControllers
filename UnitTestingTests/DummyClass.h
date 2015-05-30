9e57f9d6fd55e24d2dac1d494dd8454076df16b9
Tapan Thaker <tapan.d.thaker@gmail.com>
2015-05-30 21:57:02 +0530

    Adding a Dummy Objective-C class becuase if not added fucking XCode throws an error saying file not found on SwiftBridge Header

---
+++ UnitTestingTests/DummyClass.h
@@ -0,0 +1,13 @@
+//
+//  DummyClass.h
+//  UnitTesting
+//
+//  Created by Tapan Thaker on 30/05/15.
+//  Copyright (c) 2015 Tapan Thaker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DummyClass : NSObject
+
+@end
---
+++ UnitTestingTests/DummyClass.m
@@ -0,0 +1,13 @@
+//
+//  DummyClass.m
+//  UnitTesting
+//
+//  Created by Tapan Thaker on 30/05/15.
+//  Copyright (c) 2015 Tapan Thaker. All rights reserved.
+//
+
+#import "DummyClass.h"
+
+@implementation DummyClass
+
+@end
